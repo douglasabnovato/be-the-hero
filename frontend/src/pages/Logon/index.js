73505df5dde9b16794522a8a725893e577940694
@douglasabnovato <douglasabnovato@gmail.com>
2020-09-03 13:35:44 -0300

    v2.3.8. page logon css

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -9,16 +9,16 @@ export default function Logon(){
         <div className="logon-container">
             <section className="form">
                 <img src={ logoImg } alt="Be the Hero"/>
-            </section>
-            <form>
-                <h1>Faça seu Logon</h1>
-                <input placeholder="seu ID"/>
-                <button type="submit">Entrar</button>
-                <a href="/register">
-                    <FiLogIn size={16} color="#E02041"/>
-                    Não tenho Cadastro.
-                </a>
-            </form>
+                <form>
+                    <h1>Faça seu Logon</h1>
+                    <input placeholder="seu ID"/>
+                    <button className="button" type="submit">Entrar</button>
+                    <a href="/register">
+                        <FiLogIn size={16} color="#E02041"/>
+                        Não tenho Cadastro.
+                    </a>
+                </form>
+            </section>            
             <img src={ heroesImg } alt="Heroes"/>
         </div>
     );
