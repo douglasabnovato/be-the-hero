efab42c8b5e494a64f8658482977cd76d2fc3c91
@douglasabnovato <douglasabnovato@gmail.com>
2020-09-21 19:55:53 -0300

    mobile styles

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -5,7 +5,11 @@ export default StyleSheet.create({
     container:{
         flex: 1,
         paddingHorizontal: 24,
-        paddingTop: Constants.statusBarHeight + 20,
-        
+        paddingTop: Constants.statusBarHeight + 20,        
+    },
+    header: {
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems: 'center'
     }
 });
