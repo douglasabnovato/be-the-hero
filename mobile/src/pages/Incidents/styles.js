ed300ec30cfa0ec8c05254a697cbdb5bcf14284c
@douglasabnovato <douglasabnovato@gmail.com>
2020-09-28 20:18:40 -0300

    v3.8.3. listagem em tela

--- mobile/src/pages/Incidents/styles.js
+++ mobile/src/pages/Incidents/styles.js
@@ -7,9 +7,33 @@ export default StyleSheet.create({
         paddingHorizontal: 24,
         paddingTop: Constants.statusBarHeight + 20,        
     },
+
     header: {
         flexDirection:'row',
         justifyContent:'space-between',
         alignItems: 'center'
-    }
+    },
+
+    headerText: {
+        fontSize: 15,
+        color: '#737380',
+    },
+
+    headerTextBold: {
+        fontWeight= 'bold',
+    },
+
+    title: {
+        fontSize: 30,
+        marginBottom: 16,
+        marginTop:48,
+        color: '#13131a',
+        fontWeight:'bold',
+    },
+
+    descript:{
+        fontSize: 16,
+        lineHeight: 24,
+        color: '#737380',
+    },
 });
